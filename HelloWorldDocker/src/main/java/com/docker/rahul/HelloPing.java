390a496a9b86b9a2c9106251f68cf9c975919c9e
Rahul Mishra <penguinmishra@gmail.com>
2021-02-09 21:09:23 +0530

    Changes hello to hi

--- HelloWorldDocker/src/main/java/com/docker/rahul/HelloPing.java
+++ HelloWorldDocker/src/main/java/com/docker/rahul/HelloPing.java
@@ -3,7 +3,7 @@ package com.docker.rahul;
 public class HelloPing {
 	public static void main(String[] args) throws InterruptedException {
 		for (int i = 0; i < 100; i++) {
-			System.out.println("Hello World " + i);
+			System.out.println("Hi World " + i);
 			Thread.sleep(1000);
 		}
 	}
